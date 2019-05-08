c147bb53983fea70b6f7fca3ab1b0a74f504c6fa
Fabricio Rocha <fabricio.r@aluno.ifsp.edu.br>
2019-05-08 17:49:29 -0300

    First Commit

---
+++ Sistema-Estagio/src/main/java/estagio/SistemaEstagioApplication.java
@@ -0,0 +1,13 @@
+package estagio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEstagioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaEstagioApplication.class, args);
+	}
+
+}
---
+++ Sistema-Estagio/src/test/java/estagio/SistemaEstagioApplicationTests.java
@@ -0,0 +1,16 @@
+package estagio;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SistemaEstagioApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
