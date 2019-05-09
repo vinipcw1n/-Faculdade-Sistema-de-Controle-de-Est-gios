da69b4ff4d8d9548a85888d6177a14a4ce3a6443
Gabriel Augusto Zutião <gabriel_zut@yahoo.com.br>
2019-05-09 11:11:34 -0300

    Criação do banco funcionando

--- Sistema-Estagio/src/main/java/estagio/model/Usuario.java
+++ Sistema-Estagio/src/main/java/estagio/model/Usuario.java
@@ -1,12 +1,17 @@
 package estagio.model;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
 public abstract class Usuario {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY )
+	@GeneratedValue(strategy = GenerationType.TABLE)
 	private Long id;
 	
 	private String nome;
